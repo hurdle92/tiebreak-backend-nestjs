54b6c3243315d28e32fee9e0de491f071cee4f68
ParkSiHyungDshare <tennisflow.contact@gmail.com>
2024-10-22 18:57:19 +0900

    feat : app 공통 config 설정 추가

---
+++ src/configs/app.config.ts
@@ -0,0 +1,29 @@
+import {
+  BadRequestException,
+  INestApplication,
+  ValidationPipe,
+  VersioningType,
+} from "@nestjs/common";
+
+/**
+ * App 세팅
+ *
+ * @param {INestApplication} app
+ */
+export function setupApp(app: INestApplication): void {
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: "v",
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      exceptionFactory: (errors) => {
+        const error = errors.map((e) => ({
+          [e.property]: e.constraints,
+        }));
+        return new BadRequestException(error);
+      },
+    }),
+  );
+}
