707200a18e4a5628f46d11d9bdf072d045fdbd43
ParkSiHyungDshare <tennisflow.contact@gmail.com>
2024-10-22 18:18:45 +0900

    feat : health check api 추가

---
+++ src/apis/health/health.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Res, HttpStatus } from "@nestjs/common";
+import { Response } from "express";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
+import { HealthService } from "./health.service";
+
+@Controller("health")
+@ApiTags("서버 상태 확인 API")
+export class HealthController {
+  constructor(private readonly appService: HealthService) {}
+
+  @Get()
+  @ApiOperation({ summary: "Health Check" })
+  healthCheck(@Res() res: Response) {
+    const result = this.appService.sendOk();
+
+    return res.status(HttpStatus.OK).send(result);
+  }
+}
