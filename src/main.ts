279b2a910580f79140024a85e936fd1439d6dc30
ParkSiHyungDshare <tennisflow.contact@gmail.com>
2024-10-24 09:59:06 +0900

    feat : app 공용 환경 설정 적용

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { setupApp } from "./configs/common";
+import { ConfigService } from "./configs";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get<ConfigService>(ConfigService);
+
+  setupApp(app);
+
+  const port = configService.get("PORT");
+
   await app.listen(3000);
+
+  console.info(`Server listening on port ${port}`);
 }
 bootstrap();
