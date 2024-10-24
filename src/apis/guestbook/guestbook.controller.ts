f6a2994242e4a53371ec726b6041ebbe75130551
ParkSiHyungDshare <tennisflow.contact@gmail.com>
2024-10-24 16:02:27 +0900

    feat : Guestbook 응답 객체 변경

--- src/apis/guestbook/guestbook.controller.ts
+++ src/apis/guestbook/guestbook.controller.ts
@@ -6,23 +6,36 @@ import {
   Patch,
   Param,
   Delete,
+  Res,
+  HttpStatus,
 } from "@nestjs/common";
 import { GuestbookService } from "./guestbook.service";
 import { CreateGuestbookDto } from "./dto/create-guestbook.dto";
 import { UpdateGuestbookDto } from "./dto/update-guestbook.dto";
+import { ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
+import { GuestbookMessage } from "./guestbook.message";
 
-@Controller("guestbook")
+@Controller({ path: "guestbook" })
+@ApiTags("방명록 API")
 export class GuestbookController {
   constructor(private readonly guestbookService: GuestbookService) {}
+
+  @Get()
+  @ApiOperation({ summary: "모든 방명록 조회" })
+  @ApiOkResponse({ description: "모든 방명록을 조회합니다." })
+  async findAll(@Res() res) {
+    const guestbooks = await this.guestbookService.findAll();
+
+    return res.status(HttpStatus.OK).json({
+      statusCode: 200,
+      message: GuestbookMessage.GET_ALL_GUESTBOOKS,
+      data: guestbooks,
+    });
+  }
 
   @Post()
   create(@Body() createGuestbookDto: CreateGuestbookDto) {
     return this.guestbookService.create(createGuestbookDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.guestbookService.findAll();
   }
 
   @Get(":id")
