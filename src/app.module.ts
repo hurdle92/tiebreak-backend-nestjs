59d1ff768a34b797df92bcb8003ee8694de6621c
ParkSiHyungDshare <tennisflow.contact@gmail.com>
2024-10-24 10:31:47 +0900

    feat : 불필요한 config module 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,12 @@ import { HealthModule } from "./apis/health/health.module";
 import { typeOrmModuleOptions } from "./database/typeorm.config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { GuestbookModule } from "./apis/guestbook/guestbook.module";
-import { ConfigModule } from "./configs";
 
 @Module({
   imports: [
     HealthModule,
     TypeOrmModule.forRoot(typeOrmModuleOptions),
     GuestbookModule,
-    ConfigModule,
   ],
 })
 export class AppModule {}
