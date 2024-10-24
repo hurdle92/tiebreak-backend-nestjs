aeaa0535b3110f4673a43ee855d63eba2ae330ac
ParkSiHyungDshare <tennisflow.contact@gmail.com>
2024-10-24 15:59:49 +0900

    doc : swagger 설정 변경

--- src/configs/common/swagger.config.ts
+++ src/configs/common/swagger.config.ts
@@ -8,11 +8,11 @@ import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
  */
 export function setupSwagger(app: INestApplication): void {
   const options = new DocumentBuilder()
-    .setTitle("NestJS Study API Docs")
-    .setDescription("NestJS Study API description")
+    .setTitle("NestJS API Docs")
+    .setDescription("NestJS API 입니다")
     .setVersion("1.0.0")
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup("api-docs", app, document);
+  SwaggerModule.setup("docs", app, document);
 }
