ec589b8acdd07e16bff3b01f28e2148ece098eb1
ParkSiHyungDshare <tennisflow.contact@gmail.com>
2024-10-25 16:06:45 +0900

    feat : topic list API 구현

--- src/apis/topics/topics.service.ts
+++ src/apis/topics/topics.service.ts
@@ -1,15 +1,27 @@
-import { Injectable } from '@nestjs/common';
-import { CreateTopicDto } from './dto/create-topic.dto';
-import { UpdateTopicDto } from './dto/update-topic.dto';
+import { Injectable } from "@nestjs/common";
+import { CreateTopicDto } from "./dto/create-topic.dto";
+import { UpdateTopicDto } from "./dto/update-topic.dto";
+import { Topic } from "./entities/topic.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class TopicsService {
+  constructor(
+    @InjectRepository(Topic) private topicRepository: Repository<Topic>,
+  ) {}
+
   create(createTopicDto: CreateTopicDto) {
-    return 'This action adds a new topic';
+    return "This action adds a new topic";
   }
 
-  findAll() {
-    return `This action returns all topics`;
+  /**
+   * 모든 토픽 정보를 조회한다.
+   *
+   * @returns {Promise<Topic[]>}
+   */
+  async findAll(): Promise<Topic[]> {
+    return await this.topicRepository.find({ relations: ["courts"] });
   }
 
   findOne(id: number) {
