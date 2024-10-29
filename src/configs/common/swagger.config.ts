54249abddefbb1de3cc08e3392ecb5d74e9f81d4
ParkSiHyungDshare <tennisflow.contact@gmail.com>
2024-10-29 17:48:45 +0900

    [feat] : vercel swagger docs setting

--- src/configs/common/swagger.config.ts
+++ src/configs/common/swagger.config.ts
@@ -15,7 +15,14 @@ export function setupSwagger(app: INestApplication): void {
 
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup("docs", app, document, {
-    customCssUrl:
+    customJs: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js",
+    ],
+    customCssUrl: [
       "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css",
+    ],
   });
 }
