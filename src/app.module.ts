a2a9e7416327ddbc139f643e1b61ec01e900b1ad
ParkSiHyungDshare <tennisflow.contact@gmail.com>
2024-11-07 12:54:51 +0900

    [feat] : base url 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from "@nestjs/common";
-import { HealthModule } from "./apis/health/health.module";
+import { HealthModule } from "src/apis/health/health.module";
 import { typeOrmModuleOptions } from "./database/typeorm.config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { GuestbookModule } from "./apis/guestbook/guestbook.module";
