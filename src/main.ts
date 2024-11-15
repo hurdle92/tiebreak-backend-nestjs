5bfc9a9fcbf32f1f7039e38d3bd9f841f4e6c33c
ParkSiHyungDshare <tennisflow.contact@gmail.com>
2024-11-15 17:31:14 +0900

    [feat] : global exception handler 추가

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,19 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { setupApp, setupSwagger } from "./configs";
 import { Logger, ValidationPipe } from "@nestjs/common";
+import { HttpExceptionFilter } from "./configs/exceptions/http-exception.filter";
 
 async function bootstrap() {
   const PORT = 3000;
 
   const app = await NestFactory.create(AppModule);
   setupApp(app);
+
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  setupSwagger(app);
+  app.useGlobalFilters(new HttpExceptionFilter());
 
+  setupSwagger(app);
   await app.listen(PORT);
 
   Logger.log(`Server listening on port ${PORT}`);
