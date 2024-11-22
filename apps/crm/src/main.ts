dfec94b76152abd52938c2e359382eba8e1249e3
ParkSiHyungDshare <tennisflow.contact@gmail.com>
2024-11-22 15:49:48 +0900

    [feat] : CRM port 설정

--- apps/crm/src/main.ts
+++ apps/crm/src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from "@nestjs/core";
 import { CrmModule } from "./crm.module";
 
 async function bootstrap() {
+  const PORT = 3000;
   const app = await NestFactory.create(CrmModule);
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
