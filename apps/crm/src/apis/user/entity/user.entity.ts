7471e5af9e0e952e3f76ea5f1712ab4a941d3f6c
ParkSiHyungDshare <tennisflow.contact@gmail.com>
2024-11-22 17:44:17 +0900

    [feat] : CRM User entity 생성

---
+++ apps/crm/src/apis/user/entity/user.entity.ts
@@ -0,0 +1,32 @@
+import { ApiProperty } from "@nestjs/swagger";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity("users")
+export class User {
+  @PrimaryGeneratedColumn({ type: "int8" })
+  @ApiProperty({ description: "유저 id" })
+  id: number;
+
+  @Column({ type: "text", default: "" })
+  name: string;
+
+  @CreateDateColumn({
+    type: "timestamptz",
+    default: () => "CURRENT_TIMESTAMP",
+    nullable: false,
+  })
+  created_at: Date;
+
+  @UpdateDateColumn({
+    type: "timestamptz",
+    default: () => "CURRENT_TIMESTAMP",
+    nullable: true,
+  })
+  updated_at: Date;
+}
