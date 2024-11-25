dd9de85477d1c1d9b8ab27efafa9f516bafdcf2a
ParkSiHyungDshare <tennisflow.contact@gmail.com>
2024-11-25 11:40:27 +0900

    [feat] : auth module 추가

--- apps/crm/src/app.module.ts
+++ apps/crm/src/app.module.ts
@@ -3,12 +3,14 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { typeOrmModuleOptions } from "./database/typeorm.config";
 import { HealthModule } from "./apis/health/health.module";
 import { UserModule } from "./apis/user/user.module";
+import { AuthModule } from "./apis/auth/auth.module";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmModuleOptions),
     HealthModule,
     UserModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
