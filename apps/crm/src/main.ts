4322dc7bfb25a8ef6cd7d7b70dead34352dcda7f
ParkSiHyungDshare <tennisflow.contact@gmail.com>
2024-11-25 11:40:05 +0900

    [feat] : crm 기본적인 app 셋팅

--- apps/crm/src/main.ts
+++ apps/crm/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { setupApp, setupSwagger } from "./configs";
+import { Logger, ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-  const PORT = 3000;
+  const PORT = 4000;
+
   const app = await NestFactory.create(AppModule);
+  setupApp(app);
+
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  setupSwagger(app);
   await app.listen(PORT);
+
+  Logger.log(`Server listening on port ${PORT}`);
 }
 bootstrap();
