26db4ed3a7e59db6b5cfc61b6d880af5a69f6c80
shpkc <tennisflow.contact@gmail.com>
2024-11-25 20:37:41 +0900

    [feat] : Lesson illustration column 추가

--- apps/tiebreak/src/apis/lesson/entities/lesson/lesson.entity.ts
+++ apps/tiebreak/src/apis/lesson/entities/lesson/lesson.entity.ts
@@ -12,7 +12,10 @@ import { Court } from "../../../courts/entities/court.entity";
 import { User } from "../../../users/entities/user.entity";
 import { LessonCoreBridge } from "../lesson_core/lesson-core-bridge.entity";
 import { LessonTimeBridge } from "../lesson_time/lesson-time-bridge.entity";
-import { LESSON_ICON_DEFAULT_THUMBNAIL } from "../../../../configs/constants/links";
+import {
+  LESSON_ICON_DEFAULT_THUMBNAIL,
+  LESSON_ILLUSTRATION_DEFAULT,
+} from "../../../../configs/constants/links";
 
 @Entity("lesson")
 export class Lesson {
@@ -23,6 +26,11 @@ export class Lesson {
     default: LESSON_ICON_DEFAULT_THUMBNAIL,
   })
   icon_thumbnail: string;
+
+  @Column("text", {
+    default: LESSON_ICON_DEFAULT_THUMBNAIL,
+  })
+  lesson_illustration: string;
 
   @Column("text", { default: "" })
   good_comment: string;
@@ -86,6 +94,7 @@ export class Lesson {
   ) {
     const lesson = new Lesson();
     lesson.icon_thumbnail = LESSON_ICON_DEFAULT_THUMBNAIL;
+    lesson.lesson_illustration = LESSON_ILLUSTRATION_DEFAULT;
     lesson.good_comment = good_comment;
     lesson.bad_comment = bad_comment;
     lesson.coach_comment = coach_comment;
--- apps/tiebreak/src/database/migrations/1732533204700-Migration.ts
+++ apps/tiebreak/src/database/migrations/1732533204700-Migration.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Migration1732460215685 implements MigrationInterface {
-    name = 'Migration1732460215685'
+export class Migration1732533204700 implements MigrationInterface {
+    name = 'Migration1732533204700'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "lesson" ADD "icon_thumbnail" text NOT NULL DEFAULT 'https://gsqubspdsrnvkpdpmbyg.supabase.co/storage/v1/object/public/assets/tennis_ball_lesson.png'`);
