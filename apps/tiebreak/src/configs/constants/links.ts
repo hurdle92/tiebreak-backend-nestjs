6ca674b325fac923b6ac40dc2e8890cb2de70100
shpkc <tennisflow.contact@gmail.com>
2024-11-25 21:02:48 +0900

    [feat] : illustartion url 변경

--- apps/tiebreak/src/configs/constants/links.ts
+++ apps/tiebreak/src/configs/constants/links.ts
@@ -1,4 +1,4 @@
 export const LESSON_ICON_DEFAULT_THUMBNAIL =
   "https://gsqubspdsrnvkpdpmbyg.supabase.co/storage/v1/object/public/assets/tennis_ball_lesson.png";
 export const LESSON_ILLUSTRATION_DEFAULT =
-  "https://gsqubspdsrnvkpdpmbyg.supabase.co/storage/v1/object/public/assets/lesson_illustration.png";
+  "https://gsqubspdsrnvkpdpmbyg.supabase.co/storage/v1/object/public/assets/lesson_illustration_purple.png";
