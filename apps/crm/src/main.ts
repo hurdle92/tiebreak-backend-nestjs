2e0b3349f44454572ca461e39aae8386d0010fc0
shpkc <tennisflow.contact@gmail.com>
2024-12-04 01:18:40 +0900

    [feat] : vercel 배포 header 설정

--- apps/crm/src/main.ts
+++ apps/crm/src/main.ts
@@ -9,7 +9,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   setupApp(app);
 
-  app.enableCors();
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Accept");
+    next();
+  });
+
+  app.enableCors({
+    allowedHeaders: "*",
+    origin: "*",
+  });
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
