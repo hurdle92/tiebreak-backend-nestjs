7c22afed5bb0cebbe842879c572ed017c3075968
ParkSiHyungDshare <tennisflow.contact@gmail.com>
2024-12-13 14:53:15 +0900

    [feat] : localhost 8080 origin에 추가

--- apps/crm/src/main.ts
+++ apps/crm/src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "http://localhost:8080"],
     credentials: true,
   });
 
