84827085094292775ebda7c50153d0bfbe39c7da
shpkc <tennisflow.contact@gmail.com>
2024-12-31 01:39:44 +0900

    [feat] : game response dto에 result 추가

--- apps/crm/src/apis/game/entities/dto/response/game-response.dto.ts
+++ apps/crm/src/apis/game/entities/dto/response/game-response.dto.ts
@@ -3,6 +3,7 @@ import { GameType } from "../../game-type.enum";
 import { Game } from "../../game.entity";
 import { TeamResponseDto } from "../../../../team/entities/dto/response/team-response.dto";
 import { MeetingGameCourtOption } from "apps/crm/src/apis/meeting/entities/meeting-game-court-option/meeting-game-court-option.entity";
+import { GameResultResponseDto } from "../../../../result/entities/game-result/dto/response/game-result-response.dto";
 
 export class GameResponseDto {
   id: number;
@@ -11,6 +12,7 @@ export class GameResponseDto {
   game_type: GameType;
   meeting_game_court_option: MeetingGameCourtOption;
   teams: TeamResponseDto[];
+  game_result: GameResultResponseDto;
   created_at: Date;
   updated_at: Date;
 
@@ -21,6 +23,9 @@ export class GameResponseDto {
     this.game_type = game.game_type;
     this.meeting_game_court_option = game.meeting_game_court_option;
     this.teams = game.teams.map((team) => new TeamResponseDto(team));
+    this.game_result = game.game_results
+      ? new GameResultResponseDto(game.game_results[0])
+      : null;
     this.created_at = game.created_at;
     this.updated_at = game.updated_at;
   }
